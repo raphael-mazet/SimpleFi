72aa5d32f0f16bcc6436d32adecacf218803ee5e
Raphael <raphael.mazet@gmail.com>
2020-10-28 15:15:58 +0000

    models: first refactor with camelCase to getTokens

--- server/models/tokens.js
+++ server/models/tokens.js
@@ -1,15 +1,28 @@
 const pool = require ('./pool');
 const path = require ('path');
+const { PrismaClient } = require("@prisma/client");
 
-async function getTokens () {
+const prisma = new PrismaClient();
+
+async function getTokens() {
   try {
-    const tokens = await pool.query('select * from token');
-    return tokens.rows;
-
-  } catch (err) {
+    const tokens = await prisma.token.findMany();
+    console.log(' ---> tokens from models', tokens);
+    return tokens;
+  } catch(err) {
     console.error(`Error at ${path.basename(__dirname)}/${path.basename(__filename)} ${err}`);
-  } 
+  }
 }
+
+// async function getTokens () {
+//   try {
+//     const tokens = await pool.query('select * from token');
+//     return tokens.rows;
+
+//   } catch (err) {
+//     console.error(`Error at ${path.basename(__dirname)}/${path.basename(__filename)} ${err}`);
+//   } 
+// }
 
 async function selectUserFieldTokens(queryStr) {
   try {
