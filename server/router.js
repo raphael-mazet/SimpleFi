4df32c162a477e8f9a6f961da6aa28ed0c9991a2
Raphael <raphael.mazet@gmail.com>
2020-10-28 17:39:48 +0000

    models: fully refactor db queries with Prisma and camelCase

--- server/router.js
+++ server/router.js
@@ -5,6 +5,5 @@ const controllers = require('./controllers');
 router.get('/tokens', controllers.getTokens);
 router.get('/fields', controllers.getFields);
 router.get('/userfieldtokens/:tokenIds', controllers.getUserFieldTokens);
-router.get('/prismatest', controllers.getTokensPrisma);
 
 module.exports = router;
