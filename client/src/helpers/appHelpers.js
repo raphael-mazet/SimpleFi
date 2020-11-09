2a438cf4def4ee43324c23acc7859ac3599e8540
Raphael <raphael.mazet@gmail.com>
2020-11-09 16:13:36 +0000

    helpers: refactor to add seedToken position in field populate from cache helper

--- client/src/helpers/appHelpers.js
+++ client/src/helpers/appHelpers.js
@@ -1,11 +1,16 @@
 function populateFieldTokensFromCache (fieldsWithBalance, trackedTokens) {
   fieldsWithBalance = fieldsWithBalance.map(field => {
+
     field.cropTokens = field.cropTokens.map(token => {
       return trackedTokens.find(trackedToken => token.tokenId === trackedToken.tokenId)
     });
     
     field.seedTokens = field.seedTokens.map(token => {
-      return trackedTokens.find(trackedToken => token.tokenId === trackedToken.tokenId)
+      const { seedPosition } = token;
+      return {
+        ...trackedTokens.find(trackedToken => token.tokenId === trackedToken.tokenId),
+        seedPosition
+      }
     });
     
     return field;
