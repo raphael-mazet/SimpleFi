5fa795ba4240789a979c894b2c1e0a3bb1ab512b
Raphael <raphael.mazet@gmail.com>
2020-11-10 13:40:56 +0000

    db: change seedPosition to seedIndex

--- server/models/fields.js
+++ server/models/fields.js
@@ -8,7 +8,7 @@ async function getFields() {
         seedTokens: {
           select: {
             tokenId: true,
-            seedPosition: true
+            seedIndex: true
           }
         },
         cropTokens: {
