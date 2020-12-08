2b0769188c78cbe9e27cf979baa2b6970a4a2abf
Raphael <raphael.mazet@gmail.com>
2020-12-08 11:44:45 +0000

    comment cleanup

--- client/src/apis/ethereum/getROIs/farmingROIs/getUniswapFarmingPriceHistory.js
+++ client/src/apis/ethereum/getROIs/farmingROIs/getUniswapFarmingPriceHistory.js
@@ -1,5 +1,12 @@
 import { getUniswapBalanceHistory } from "../../protocolQueries";
 
+/**
+ * @param {Object} tx - currently analysed user ERC20 transaction (fetched from Etherscan)
+ * @param {String} userAccount
+ * @dev - this assumes that the data from the Graph on the user's Uniswap balances will always match Etherscan's user ERC20 tx data
+ *        (it should as the Graph records balance changes on block within which user staked/unstaked the receipt token)
+ * @return {Number} - price of the uniswap receipt token at the time of the transaction
+ */
 async function getOneUniswapHistReceiptPrice (tx, userAccount) {
   const rawData = await getUniswapBalanceHistory(userAccount);
   const targetBlock = rawData.data.liquidityPositionSnapshots.find(data => data.block === Number(tx.tx.blockNumber));
