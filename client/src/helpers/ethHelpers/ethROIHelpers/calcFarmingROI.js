05a39583aa3cd9f454ed9b92d6650e913cda722b
Raphael <raphael.mazet@gmail.com>
2020-12-24 12:50:10 +0100

    Farming ROI: refactor calcs for a hist ROI based on weighted investments by time

--- client/src/helpers/ethHelpers/ethROIHelpers/calcFarmingROI.js
+++ client/src/helpers/ethHelpers/ethROIHelpers/calcFarmingROI.js
@@ -7,13 +7,17 @@
  */
 function calcFarmingROI (txHistory, userTokens, tokenPrices, field) {
   const {cropTokens, fieldId} = field;
+  //registers value at time of claim only - assume return is realised at that moment (no calc of what it's worth today if user held onto it)
   let valueClaimed = 0;
   let valueUnclaimed = 0;
-  let valueInvested = 0;
-  let valueRealised = 0;
-  // const invArray = [{amount, date}]
+  // let valueInvested = 0;
+  // let valueRealised = 0;
+  //used to record the amount of time an investment was made for (value indicates value in $. Amount indicates token amount (un)staked)
+  //currentInv.amount not currently used in calcs, but may be at a later date
+  const currentInv = {value: 0, amount: 0, dateStart: null};
+  const weightedInvestments =[];
 
-
+  console.log(' ---> field.name', field.name);
   //@dev: [{tx, [crop | receipt]Token, [priceApi,] [reward | staking | unstaking]Amount, pricePerToken}]
   txHistory.forEach(userTx => {
     if (field.name === "SNX: curve sUSD rewards") console.log(' ---> userTx', userTx);
@@ -21,26 +25,58 @@ function calcFarmingROI (txHistory, userTokens, tokenPrices, field) {
     if (rewardAmount) {
       valueClaimed += rewardAmount * pricePerToken;
     } else if (stakingAmount) {
-      valueInvested += stakingAmount * pricePerToken;
+      if (currentInv.value) {
+        const stakedTime = userTx.txDate - currentInv.dateStart;
+        weightedInvestments.push({amount: currentInv.amount, value: currentInv.value, stakedTime});
+      }
+        currentInv.value += stakingAmount * pricePerToken;
+        currentInv.amount += stakingAmount;
+        currentInv.dateStart = userTx.txDate;
+      // valueInvested += stakingAmount * pricePerToken;
     } else if (unstakingAmount) {
-      valueRealised += unstakingAmount * pricePerToken;
+      const stakedTime = userTx.txDate - currentInv.dateStart;
+      weightedInvestments.push({amount: currentInv.amount, value: currentInv.value, stakedTime});
+      //set currentInv value to 0 if it's just dust
+      if ((currentInv.value - (unstakingAmount * pricePerToken)) < 1) {
+        currentInv.value = 0;
+        currentInv.amount = 0;
+        currentInv.dateStart = null;
+      } else {
+        currentInv.value -= unstakingAmount * pricePerToken;
+        currentInv.amount -= unstakingAmount;
+        currentInv.dateStart = userTx.txDate;
+      }
+      // valueRealised += unstakingAmount * pricePerToken;
     }
   })
+  
+  //set any outstanding investment as having lasted till today's date
+  if (currentInv.value) {
+    const stakedTime = new Date() - currentInv.dateStart;
+    weightedInvestments.push({amount: currentInv.amount, value: currentInv.value, stakedTime});
+  }
+  console.log(' ---> weightedInvestments', weightedInvestments);
+  const reducedInvestmentWeights = weightedInvestments.reduce((acc, curr) => {
+    acc.investments += curr.value * curr.stakedTime;
+    acc.totalTime += curr.stakedTime;
+    return acc;
+  }, {investments: 0, totalTime: 0});
+  console.log(' ---> reducedInvestmentWeights', reducedInvestmentWeights);
+  const avgInvestment = reducedInvestmentWeights.investments / reducedInvestmentWeights.totalTime;
+  console.log(' ---> avgInvestment', avgInvestment);
 
   const targetCropTokens = userTokens.filter(userToken => {
     return cropTokens.some(cropToken => cropToken.tokenId === userToken.tokenId);
   });
   targetCropTokens.forEach(token => {
+    console.log(' ---> token.name', token.name);
     valueUnclaimed += token.unclaimedBalance.reduce((acc, curr) => {
       return curr.field.fieldId === fieldId ? curr.balance * tokenPrices[token.name].usd : acc;
     }, valueUnclaimed)
+    console.log(' ---> valueUnclaimed', valueUnclaimed);
   })
 
-  //TODO: alt ROI based on current crop value (investmentValue vs. amountInvested) for toggling in Field details
-  //FIXME: absolutely necessary
-  //alternative: add investmentValue in function args (currInvestmentValue from getROIs)
-  //old calc: return (investmentValue + amountUnclaimed + amountClaimed + amountRealised) / amountInvested;
-  return (valueUnclaimed + valueClaimed + valueRealised) / valueInvested;    
+  return (valueUnclaimed + valueClaimed) / avgInvestment;    
 }
 
 export default calcFarmingROI;
