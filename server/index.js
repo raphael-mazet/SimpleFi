929acbfeafccaf1f0110bcc8aa532ad924d30816
Raphael <raphael.mazet@gmail.com>
2021-01-15 14:57:58 +0000

    add cors

--- server/index.js
+++ server/index.js
@@ -8,9 +8,14 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3020;
 
+const corsConfig = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
 app.use(morgan('tiny'));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(router);
 
 app.listen(port, () => {
