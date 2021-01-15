0dac0b91b44ffa6e0f235732a97c84e57f2a5f81
Raphael <raphael.mazet@gmail.com>
2021-01-15 16:34:02 +0000

    .env change

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = require('./router');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path')
 
 require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT || 3020;
+const port = process.env.PORT;
 
 const corsConfig = {
   origin: 'http://localhost:3000',
@@ -20,4 +21,5 @@ app.use(router);
 
 app.listen(port, () => {
   console.log(`Solo server listening on localhost:${port} 🎉`)
+  console.log(' ---> the path', path.resolve(__dirname, './prisma/.env'));
 });
