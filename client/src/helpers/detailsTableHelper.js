c2038d9f9b435ec45027edb3bfde6bbc40794202
Raphael <raphael.mazet@gmail.com>
2021-01-19 16:01:51 +0000

    fix valueless tx bug in details helper

--- client/src/helpers/detailsTableHelper.js
+++ client/src/helpers/detailsTableHelper.js
@@ -16,14 +16,14 @@ export default function extractTempFieldDetailsCells(tx) {
 function addTxTypeAndAmount(tx) {
     const { txIn, txOut, staked, unstaked, stakingAmount, unstakingAmount, rewardAmount} = tx;
     //@dev: txIn(out) are earning field txs
-    if (txIn) return {action: 'accumulated', amount: txIn, balEffect: 'plus'};
-    else if (txOut) return {action: 'exited', amount: txOut, balEffect: 'minus'};
+    if (txIn || txIn === 0) return {action: 'accumulated', amount: txIn, balEffect: 'plus'};
+    else if (txOut || txOut === 0) return {action: 'exited', amount: txOut, balEffect: 'minus'};
     /* @dev: (un)staked are an earning field tx where its receipt token interacts with a
              farming field and therefore has no impact on the underlying earning field balance */
-    else if (staked) return {action: 'staked', amount: staked, balEffect: 'neutral'};
-    else if (unstaked) return {action: 'unstaked', amount: unstaked, balEffect: 'neutral'};
+    else if (staked || staked === 0) return {action: 'staked', amount: staked, balEffect: 'neutral'};
+    else if (unstaked || unstaked === 0) return {action: 'unstaked', amount: unstaked, balEffect: 'neutral'};
     //@dev (un)stakingAmount are farming field txs and therefore modify the farming field's balance
-    else if (stakingAmount) return {action: 'staked', amount: stakingAmount, balEffect: 'plus'};
-    else if (unstakingAmount) return {action: 'unstaked', amount: unstakingAmount, balEffect: 'minus'};
-    else if (rewardAmount) return {action: 'claimed', amount: rewardAmount, balEffect: 'neutral'}
+    else if (stakingAmount || stakingAmount === 0) return {action: 'staked', amount: stakingAmount, balEffect: 'plus'};
+    else if (unstakingAmount || unstakingAmount === 0) return {action: 'unstaked', amount: unstakingAmount, balEffect: 'minus'};
+    else if (rewardAmount || rewardAmount === 0) return {action: 'claimed', amount: rewardAmount, balEffect: 'neutral'}
   };
